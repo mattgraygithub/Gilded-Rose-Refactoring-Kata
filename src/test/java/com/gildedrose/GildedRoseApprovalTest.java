93cdbc2c9b907ccb31aefb442d850dc5e6068e5e
codurance2 <mattgray83@hotmail.co.uk>
2019-09-11 13:31:35 +0100

    Refactored approval test - now has 1 million test cases

--- src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -11,38 +11,9 @@ public class GildedRoseApprovalTest {
     @Test
     public void updateQuality() {
 
-        List<String> namesList = new ArrayList<String>() {{
-            add("Aged Brie");
-            add("Backstage passes to a TAFKAL80ETC concert");
-            add("Sulfuras, Hand of Ragnaros");
-        }};
-
-        List<Integer> sellInsList = new ArrayList<>();
-        List<Integer> qualitiesList = new ArrayList<>();
-
-        StringBuilder name = new StringBuilder("Item");
-
-        int sellIn = 50;
-
-        for (int i = 0; i < 100; i++) {
-            name.append(i);
-            namesList.add(name.toString());
-
-            sellInsList.add(sellIn);
-            sellIn --;
-
-            qualitiesList.add(i);
-            name = new StringBuilder("Item");
-        }
-
-        String[] names = new String[namesList.size()];
-        names = namesList.toArray(names);
-
-        Integer[] sellIns = new Integer[sellInsList.size()];
-        sellIns = sellInsList.toArray(sellIns);
-
-        Integer[] qualities = new Integer[qualitiesList.size()];
-        qualities = qualitiesList.toArray(qualities);
+        String[] names = generateNames();
+        Integer[] sellIns = generateSellIns();
+        Integer[] qualities = generateQualities();
 
         CombinationApprovals.verifyAllCombinations(
                 this::doUpdateQuality,
@@ -56,5 +27,46 @@ public class GildedRoseApprovalTest {
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         return app.items[0].toString();
+    }
+
+    private String[] generateNames() {
+        List<String> namesList = new ArrayList<String>() {{
+            add("Aged Brie");
+            add("Backstage passes to a TAFKAL80ETC concert");
+            add("Sulfuras, Hand of Ragnaros");
+        }};
+
+        StringBuilder name = new StringBuilder("Item");
+        for (int i = 0; i < 97; i++) {
+            name.append(i);
+            namesList.add(name.toString());
+            name = new StringBuilder("Item");
+        }
+
+        String[] names = new String[namesList.size()];
+        names = namesList.toArray(names);
+        return names;
+    }
+
+    private Integer[] generateSellIns() {
+        List<Integer> sellInsList = new ArrayList<>();
+        for (int i = -50; i < 50; i++) {
+            sellInsList.add(i);
+        }
+
+        Integer[] sellIns = new Integer[sellInsList.size()];
+        sellIns = sellInsList.toArray(sellIns);
+        return sellIns;
+    }
+
+    private Integer[] generateQualities() {
+        List<Integer> qualitiesList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            qualitiesList.add(i);
+        }
+
+        Integer[] qualities = new Integer[qualitiesList.size()];
+        qualities = qualitiesList.toArray(qualities);
+        return qualities;
     }
 }
